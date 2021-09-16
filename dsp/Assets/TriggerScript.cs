7e0041198ac654a63a513bb1ad3ca4a201e14128
Boogaart <zboogaart@ufl.edu>
2021-09-16 17:11:39 -0400

    Developed training task with green cubes. Still need to write data for time cubes are found and locations of cubes.

---
+++ dsp/Assets/TriggerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    public Collider person;
+
+    public int nextCubeNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider person){
+      if(person.gameObject.name.Contains("Player")){
+          Debug.Log("Collision");
+          PlayerPrefs.SetInt("trainingTask",nextCubeNum);
+          //PlayerPrefs.SetFloat("cubeLocation",transform.position);
+      }
+
+  	}
+}
